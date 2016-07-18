d86a5b03898ccbf05af02f26a39f1698c237d75b
G04 <clooveer4@gmail.com>
2016-07-18 23:36:05 +0900

    add:投稿画面（main.php）への投稿機能（mlkcca）の追加
    ＜追加＞
    ・投稿画面（main.php） の製作のため、mlkccaを使用し簡易的な投稿内容（文字）入力と投稿内容（文字）の表示を追加。
    ＜他＞
    ・ログイン画面でphp使用でのauth0の実装を試したが上手くいかず。。。（3h程...）
    →issueにて質問します。

--- tanaka/main.php
+++ tanaka/main.php
@@ -1,33 +1,113 @@
-<?php
-# 認証を要求したいページの先頭に以下を記述します。
-require_once './signup_index.php';
-?>
-    <!DOCTYPE html>
-    <html lang="ja">
+<!DOCTYPE html>
+<html lang="ja">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <title>Local Life - ローカルライフ</title>
-        <link rel="stylesheet" type="text/css" href="css/signup.css">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0">
+    <title>Local Life - ローカルライフ</title>
+    <link rel="stylesheet" type="text/css" href="css/signup.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js">
+    </script>
+    <script src="https://cdn.mlkcca.com/v0.6.0/milkcocoa.js">
+    </script>
 
-    <body>
-        <header>
-           <h1>Local Life</h1>
-            <p>
-                <?php echo h($_SESSION['username']); ?>
-            </p>
-            <p>
+    <script>
+        $(function () {
+            //1.ミルクココアインスタンスを作成
+            var milkcocoa = new MilkCocoa("flagiqs2dulo.mlkcca.com");
+
+            //2."lol"データストアを作成
+            var ds = milkcocoa.dataStore("lol");
+
+            //3."lol"データストアからメッセージを取ってくる
+            ds.stream().sort("desc").next(function (err, datas) {
+                datas.forEach(function (data) {
+                    renderMessage(data);
+                });
+            });
+
+            //4."lol"データストアのプッシュイベントを監視
+            ds.on("push", function (e) {
+                renderMessage(e);
+            });
+
+            var last_message = "dummy";
+
+            function renderMessage(message) {
+                var message_html = '<p class="post-text">' + escapeHTML(message.value.content) + '</p>';
+                var date_html = '';
+                if (message.value.date) {
+                    date_html = '<p class="post-date">' + escapeHTML(new Date(message.value.date).toLocaleString()) + '</p>';
+                }
+                $("#" + last_message).before('<div id="' + message.id + '" class="post">' + message_html + date_html + '</div>');
+                last_message = message.id;
+            }
+
+            function post() {
+            //5."lol"データストアにメッセージをプッシュする
+                var content = escapeHTML($("#content").val());
+                if (content && content !== "") {
+                    ds.push({
+                        title: "タイトル",
+                        content: content,
+                        date: new Date().getTime()
+                    }, function (e) {});
+                }
+                $("#content").val("");
+            }
+
+            $('#post').click(function () {
+                post();
+            })
+            $('#content').keydown(function (e) {
+                if (e.which == 13) {
+                    post();
+                    return false;
+                }
+            });
+        });
+        //インジェクション対策
+        function escapeHTML(val) {
+            return $('<div>').text(val).html();
+        };
+    </script>
+
+
+</head>
+
+<body>
+    <header>
+        <h1>Local Life</h1>
+        <p>
+
+        </p>
+        <p>
             <a href="signup_logout.php">ログアウト</a>
-            </p>
-        </header>
-        <main>
-        
-        </main>
-        <footer>
-            Local life ©2016
-        </footer>
-    </body>
+        </p>
+    </header>
 
-    </html>
+    <main>
+
+        <div class="container">
+            <div class="postarea cf">
+                <div class="postarea-text">
+                    <textarea name="" id="content" cols="50" rows="3
+                " placeholder="投稿する"></textarea>
+                </div>
+                <button id="post" class="postarea-button">投稿する</button>
+            </div>
+        </div>
+        <div id="messages" class="content">
+            <div id="dummy">
+
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        Local life ©2016
+    </footer>
+</body>
+
+</html>
